acc3a003b542bc9ebc4dbfcdf499b18571b5b4df
MartinKay <martinkay@qq.com>
2023-11-10 09:19:35 +0800

    feat: version upgrade to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.4"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
