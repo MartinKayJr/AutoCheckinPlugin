0d573740ab406586909f15eeea1ee09a52c5937c
MartinKay <martinkay@qq.com>
2024-10-12 10:58:55 +0800

    feat: 升级为2.3版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cn.martinkay.autocheckinplugin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
